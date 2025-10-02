838469d2de6a308a4eb23d7220f355d1edf084ef
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-10-02 14:59:22 -0400

    Update scramjet.config.js

--- public/static/$/scramjet.config.js
+++ public/static/$/scramjet.config.js
@@ -4,8 +4,7 @@ self.__scramjet$config = {
   prefix: "/$/daydream/",
   files: {
       wasm: "/$/scramjet.wasm.wasm",
-      wasm: "/$/scramjet.wasm.js",
-      worker: "/$/scramjet.worker.js",
+      worker: "/$/scramjet.all.js",
       client: "/$/scramjet.client.js",
       shared: "/$/scramjet.shared.js",
       sync: "/$/scramjet.sync.js"
