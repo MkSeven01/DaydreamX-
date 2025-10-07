bc0d2007c8bd886d0994d93a1e7f7abe7702bbbb
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-10-07 11:44:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ const port = process.env.PORT || 2345, server = createServer(), bare = process.e
 logging.set_level(logging.NONE);
 
 Object.assign(wisp.options, {
-  dns_method: "resolve",
-  dns_servers: ["1.1.1.3", "1.0.0.3"],
-  dns_result_order: "ipv4first",
+  dns_method: 'resolve',
+  dns_servers: ['1.1.1.3', '1.0.0.3'],
+  dns_result_order: 'ipv4first',
+  wisp_version: 2;,
+  wisp_motd: "WISP server";
 });
 
 server.on("upgrade", (req, sock, head) =>
