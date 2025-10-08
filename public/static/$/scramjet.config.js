d9f6ea8b3a4b83a1945906b1fafd0f88c355898f
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-10-08 10:52:21 -0400

    Update scramjet.config.js

--- public/static/$/scramjet.config.js
+++ public/static/$/scramjet.config.js
@@ -196,7 +196,7 @@ flags: {
       }
       if (url.pathname.includes("ads.js") ||
         url.pathname.includes("pagead.js") ||
-		url.pathname.includes("ad status.js") ||
+		url.pathname.includes("ad.status.js") ||
         url.pathname.includes("partner.ads.js")) {
         return new Response(null, {});
       }
