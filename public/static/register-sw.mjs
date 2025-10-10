915469b4214fcf1fe9779ee1347cf5a678b10691
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-10-10 09:44:08 -0400

    Update register-sw.mjs

--- public/static/register-sw.mjs
+++ public/static/register-sw.mjs
@@ -4,6 +4,7 @@
  */
 const stockSW = "/static/@/uv.sw.js";
 const stockSW = "/static/e/eclipse.worker.js";
+const stockSW = "/static/$/scramjet.shared.js";
 
 /**
  * List of hostnames that are allowed to run serviceworkers on http://
